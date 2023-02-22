008870823a1b6895530f3ae811d41cd9c3ac6164
song <songyw9812@gmail.com>
2023-02-22 19:08:03 +0900

    :sparkles: implement slack operation

--- src/slack/slack.module.ts
+++ src/slack/slack.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SlackService } from './slack.service';
 import { SlackController } from './slack.controller';
+import { HttpModule } from '@nestjs/axios';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [SlackController],
-  providers: [SlackService],
+  providers: [SlackService, PrismaService],
+  imports: [HttpModule],
 })
 export class SlackModule {}
--- src/slack/slack.service.ts
+++ src/slack/slack.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSlackDto } from './dto/create-slack.dto';
 import { UpdateSlackDto } from './dto/update-slack.dto';
+import { Cron } from '@nestjs/schedule';
+import { PrismaService } from '../prisma.service';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class SlackService {
+  constructor(
+    private prisma: PrismaService,
+    private httpService: HttpService,
+  ) {}
+
   create(createSlackDto: CreateSlackDto) {
     return 'This action adds a new slack';
   }
@@ -22,5 +30,31 @@ export class SlackService {
 
   remove(id: number) {
     return `This action removes a #${id} slack`;
+  }
+
+  @Cron('0 0 11 * * *', {
+    timeZone: 'Asia/Seoul',
+  })
+  async notice() {
+    const day = await this.prisma.birthday.findMany({
+      where: {
+        birthday: new Date(),
+      },
+    });
+
+    for (const d of day) {
+      await this.httpService.post(
+        'https://slack.com/api/chat.postMessage',
+        {
+          channel: process.env.CHANNEL_NAME,
+          text: `오늘은 ${d.name} 청년의 생일입니다. 모두 축하해주세요!`,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+          },
+        },
+      );
+    }
   }
 }
