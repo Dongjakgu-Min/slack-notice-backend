7589ea93774e47dcdfb19183305db8b0900caf6a
song <songyw9812@gmail.com>
2023-03-01 17:54:16 +0900

    :sparkles: Add Lunar Calendar check

--- src/slack/slack.service.ts
+++ src/slack/slack.service.ts
@@ -4,6 +4,7 @@ import { UpdateSlackDto } from './dto/update-slack.dto';
 import { Cron } from '@nestjs/schedule';
 import { PrismaService } from '../prisma.service';
 import { HttpService } from '@nestjs/axios';
+import { CalendarKorean } from 'date-chinese';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
@@ -38,14 +39,28 @@ export class SlackService {
   })
   async notice() {
     const today = new Date();
+    const lunar = new CalendarKorean();
+    const lunarToday = lunar.fromDate(today);
+
     const day: any = await this.prisma
       .$queryRaw`SELECT * FROM Birthday WHERE (DATE_FORMAT(birthday, '%m')=${(
       '0' + (today.getMonth() + 1).toString()
     ).slice(-2)} AND DATE_FORMAT(birthday, '%d')=${(
       '0' + today.getDate().toString()
-    ).slice(-2)})`;
+    ).slice(-2)} AND isLunar=false)`;
 
-    for (const d of day) {
+    // CalendarKorean의 get 메소드는 [cycle, year, month, leap, day] 순으로 출력
+    // 우리가 필요한 건 month와 day이므로 그 부분만 추출해서 사용
+    const dayLunar: any = await this.prisma
+      .$queryRaw`SELECT * FROM Birthday WHERE (DATE_FORMAT(birthday, '%m')=${(
+      '0' + lunarToday.get()[2]
+    ).slice(-2)} AND DATE_FORMAT(birthday, '%d')=${(
+      '0' + lunarToday.get()[4]
+    ).slice(-2)} AND isLunar=false)`;
+
+    const birthday = [...day, ...dayLunar];
+
+    for (const d of birthday) {
       const result = await firstValueFrom(
         this.httpService.post(
           'https://slack.com/api/chat.postMessage',
